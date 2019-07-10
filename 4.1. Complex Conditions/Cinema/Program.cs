3afb4aab50617a4e39a242d0f8046d74b817d25f
Eleonor Manolova <eleonor.manolova@gmail.com>
2019-07-10 14:26:40 +0300

    Refactored code

---
+++ 4.1. Complex Conditions/Cinema/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Cinema
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var type = Console.ReadLine().ToLower();
+            var row = double.Parse(Console.ReadLine());
+            var column = double.Parse(Console.ReadLine());
+            var price = 0.0;
+
+            switch (type)
+            {
+                case "premiere":
+                    price = row * column * 12.0;
+                    break;
+                case "normal":
+                    price = row * column * 7.5;
+                    break;
+                case "discount":
+                    price = row * column * 5.0;
+                    break;
+            }
+
+            Console.WriteLine("{0:0.00} leva", price);
+        }
+    }
+}
---
+++ 4.1. Complex Conditions/Volleyball/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Volleyball
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var year = Console.ReadLine().ToLower();
+            var holiday = int.Parse(Console.ReadLine());
+            var weekend = int.Parse(Console.ReadLine());
+
+            var saturday = (48 - weekend) * 3 / 4.0 + 2 / 3.0 * holiday;
+            var sunday = weekend;
+
+            switch (year)
+            {
+                case "normal":
+                    Console.WriteLine(Math.Floor(saturday + sunday));
+                    break;
+                case "leap":
+                    Console.WriteLine(Math.Floor(saturday + sunday + 15 / 100.0 * (saturday + sunday)));
+                    break;
+            }
+        }
+    }
+}
